09f57b9a4d9e0076ae5ee7e855c9136f14c600f4
Thiago <gmoliveira69@gmail.com>
2022-05-21 21:07:43 -0300

    create car service

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,6 +5,8 @@ import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/C
 import { SpecificationRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository'
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationsRepository'
 import {container} from 'tsyringe'
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationRepository> (
 container.registerSingleton<IUsersRepository> (
     "UsersRepository",
     UsersRepository
+ )
+
+container.registerSingleton<ICarsRepository> (
+    "CarsRepository",
+    CarsRepository
  )
