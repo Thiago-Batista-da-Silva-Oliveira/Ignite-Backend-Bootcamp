cd3bba912b7ffec18f8d1f89650d0d2cd92e6461
Thiago <gmoliveira69@gmail.com>
2022-06-09 18:33:41 -0300

    tests

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../IRentalsRepository";
 
@@ -6,11 +7,25 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     retals:Rental[] = []
  
   async  findOpenRentalByCar(car_id: string): Promise<Rental> {
-      return this.retals.find(rental => rental.car_id === car_id && rental.end_date === null)
+      return this.retals.find(rental => rental.car_id === car_id && !rental.end_date)
     }
 
    async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return this.retals.find(rental => rental.user_id === user_id && rental.end_date === null)
+    return this.retals.find(rental => rental.user_id === user_id && !rental.end_date)
+    }
+
+   async create({car_id,expected_return_date,user_id}: ICreateRentalDTO): Promise<Rental> {
+        const rental = new Rental()
+        Object.assign(rental, {
+            car_id,
+            expected_return_date,
+            user_id,
+            start_date: new Date()
+        })
+
+        this.retals.push(rental)
+
+        return rental
     }
 }
 
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
 
@@ -11,7 +12,7 @@ class CreateRentalUseCase {
     constructor (
         private rentalsRepository: IRentalsRepository
     ){}
-    async execute ({user_id, expected_return_date,car_id}: IRequest) {
+    async execute ({user_id, expected_return_date,car_id}: IRequest):Promise<Rental> {
         
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id)
     
@@ -24,6 +25,14 @@ class CreateRentalUseCase {
         if (rentalOpenToUser) {
             throw new AppError("There's a rental in progress for user!")
         }
+
+        const rental = await this.rentalsRepository.create({
+            user_id,
+             car_id,
+             expected_return_date
+        })
+
+        return rental
     }
 }
 export {CreateRentalUseCase}
