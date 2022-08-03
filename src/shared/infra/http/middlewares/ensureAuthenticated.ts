f450c9233b4791a1815eeb64a9032f3717facd61
Thiago <gmoliveira69@gmail.com>
2022-08-02 21:42:16 -0300

    fixing refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -6,38 +6,30 @@ import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositor
 import auth from "@config/auth";
 
 interface IPayload {
-    sub: string
+  sub: string;
 }
 
-export async function ensureAuthenticated(request:Request, response: Response, next: NextFunction){
-    const authHeader = request.headers.authorization;
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
 
-    const usersTokensRepository = new UsersTokensRepository()
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
 
+  const [, token] = authHeader.split(" ");
 
-    if(!authHeader){
-        throw new AppError("Token missing", 401)
-    }
+  try {
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+    request.user = {
+      id: user_id,
+    };
 
-    const [, token] = authHeader.split(" ")
-
-   try{
-    const {sub: user_id} = verify(token,auth.secret_refresh_token) as IPayload
-     
-     const usersRepository = new UsersRepository()
-     const user = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token)
-
-     if(!user){
-         throw new AppError("User does not exists!", 401)
-     }
-    
-     request.user = {
-         id: user_id
-     }
-    
-    next()
-} catch{
-       throw new AppError("invalid token!", 401)
-   }
-
+    next();
+  } catch {
+    throw new AppError("invalid token!", 401);
+  }
 }
